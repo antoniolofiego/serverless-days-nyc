b4b98693f55cedaf923e99d2a741f0e0fed75318
Antonio Lo Fiego <alofiego@hey.com>
2022-01-18 19:33:50 -0500

    Shell for Navbar component

---
+++ components/Navbar/index.jsx
@@ -0,0 +1,26 @@
+export const Navbar = () => {
+  return (
+    <nav className='my-8'>
+      <ul className='flex justify-center text-xl font-title space-x-8'>
+        <li>
+          <a href=''>Home</a>
+        </li>
+        <li>
+          <a href=''>About</a>
+        </li>
+        <li>
+          <a href=''>Speakers</a>
+        </li>
+        <li>
+          <a href=''>Agenda</a>
+        </li>
+        <li>
+          <a href=''>Location</a>
+        </li>
+        <li>
+          <a href=''>Tickets</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
