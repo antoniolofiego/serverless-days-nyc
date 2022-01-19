bb555acda288b74f55654c36ddf0b26f36486ea8
Antonio Lo Fiego <alofiego@hey.com>
2022-01-18 19:33:13 -0500

    Import custom fonts in Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        title: ['Montserrat', ...defaultTheme.fontFamily.sans],
+        sans: ['Open Sans', 'sans-serif', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
