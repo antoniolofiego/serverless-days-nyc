2fd622a8bcdcdd13e0dec87861374395cd8ec848
Antonio Lo Fiego <alofiego@hey.com>
2022-01-18 19:34:12 -0500

    Shell for Speakers component

---
+++ components/Speakers/index.jsx
@@ -0,0 +1,14 @@
+import { SpeakerCard } from './SpeakerCard';
+export const Speakers = () => {
+  const speakers = [1, 2, 3, 4, 5, 6];
+  return (
+    <div className='my-8 space-y-4'>
+      <SpeakerCard />
+      <div className='grid grid-cols-1 lg:grid-cols-2 gap-6'>
+        {speakers.map((_, i) => {
+          return <SpeakerCard key={i} />;
+        })}
+      </div>
+    </div>
+  );
+};
