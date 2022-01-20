ade418074ae07d8115b569597a887d528155cd4e
Antonio Lo Fiego <alofiego@hey.com>
2022-01-20 17:01:05 -0500

    Use custom hook and resolvedTheme in ThemeSwitcherButton

--- components/ThemeSwitcherButton.jsx
+++ components/ThemeSwitcherButton.jsx
@@ -1,14 +1,18 @@
 import { MoonIcon, SunIcon } from '@heroicons/react/solid';
 import { useTheme } from 'next-themes';
+import { useMounted } from '../hooks';
 
 export const ThemeSwitcherButton = ({ height }) => {
-  const { theme, setTheme, resolvedTheme } = useTheme();
+  const mounted = useMounted();
+  const { setTheme, resolvedTheme } = useTheme();
+
+  if (!mounted) return null;
 
   return (
     <button
       data-testid='themeSwitcher'
       className={`flex items-center text-sm rounded-md group ${height}`}
-      onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+      onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
     >
       {resolvedTheme === 'dark' && (
         <SunIcon className={'h-full w-full text-gray-50'} />
