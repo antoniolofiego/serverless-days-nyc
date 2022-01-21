381e300fc69da7a0ef8b2864313774910e7a440a
Antonio Lo Fiego <alofiego@hey.com>
2022-01-21 17:41:19 -0500

    Increase heading bottom margin

--- components/Heading.jsx
+++ components/Heading.jsx
@@ -1,6 +1,6 @@
 export const Heading = ({ title }) => {
   return (
-    <div className='flex mx-auto mb-8'>
+    <div className='flex mx-auto mb-10'>
       <h2 className='pb-8 mx-auto text-4xl font-bold tracking-wide border-b-2 border-gray-900 dark:border-gray-50 font-title dark:text-gray-50 light:text-gray-900'>
         {title}
       </h2>
