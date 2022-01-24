fc5066517aa249949cb2d0c12a48c11a0ee908cd
Antonio Lo Fiego <alofiego@hey.com>
2022-01-24 01:43:05 -0500

    Move talks to file

--- components/Agenda.jsx
+++ components/Agenda.jsx
@@ -1,5 +1,6 @@
 import { Heading } from './Heading';
 import { EventCard } from './EventCard';
+import { talks } from '../data/talks';
 
 export const Agenda = () => {
   return (
@@ -9,38 +10,16 @@ export const Agenda = () => {
     >
       <Heading title='Agenda' />
       <ul className='flex flex-wrap items-stretch justify-center grid-cols-1 gap-4 mx-auto place-items-center center lg:grid-cols-2 lg:justify-start'>
-        <EventCard
-          talkName={'Build a Serverless Web Store'}
-          speakerName={'Developer Cool'}
-          description={'A cool talk about cool stuff'}
-        />
-        <EventCard
-          talkName={'Randomly talking about some longer stuff'}
-          speakerName={'Big Name In Tech'}
-          description={'A neat talk about neat stuff'}
-        />
-        <EventCard
-          talkName={
-            'Building a 100% Serverless Blog Site Application with the Right Observability'
-          }
-          speakerName={'Developer Cool'}
-        />
-        <EventCard
-          talkName={'Another mock talk for testing purposes'}
-          speakerName={'Developer Cool'}
-        />
-        <EventCard
-          talkName={'Whatever this may be'}
-          speakerName={'Developer Cool'}
-        />
-        <EventCard
-          talkName={'Cool stuff ahead, my man'}
-          speakerName={'Developer Cool'}
-        />
-        <EventCard
-          talkName={'Wowowowowow congrats on the talk'}
-          speakerName={'Developer Cool'}
-        />
+        {talks.map((talk, i) => {
+          return (
+            <EventCard
+              key={i}
+              talkName={talk.talkName}
+              speakerName={talk.speakerName}
+              id={talk.id}
+            />
+          );
+        })}
       </ul>
     </div>
   );
---
+++ data/talks.js
@@ -0,0 +1,38 @@
+export const talks = [
+  {
+    id: 0,
+    talkName: 'Test1',
+    speakerName: 'Speaker1',
+    description: 'Description1',
+  },
+  {
+    id: 1,
+    talkName: 'Test2',
+    speakerName: 'Speaker2',
+    description: 'Description2',
+  },
+  {
+    id: 2,
+    talkName: 'Test3',
+    speakerName: 'Speaker3',
+    description: 'Description3',
+  },
+  {
+    id: 3,
+    talkName: 'Test4',
+    speakerName: 'Speaker4',
+    description: 'Description4',
+  },
+  {
+    id: 4,
+    talkName: 'Test5',
+    speakerName: 'Speaker5',
+    description: 'Description5',
+  },
+  {
+    id: 5,
+    talkName: 'Test6',
+    speakerName: 'Speaker6',
+    description: 'Description6',
+  },
+];
