4d23d66d1ef715da618f043e9b383e9934ddc2b8
Antonio Lo Fiego <alofiego@hey.com>
2022-01-24 01:56:27 -0500

    Implement EventModal trigger in the EventCard

--- components/EventCard.jsx
+++ components/EventCard.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { Modal } from './Modal';
+import { EventModal } from './EventModal';
 
-export const EventCard = ({ talkName, speakerName, description }) => {
+export const EventCard = ({ talkName, speakerName, id }) => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
   const open = () => {
@@ -19,11 +20,11 @@ export const EventCard = ({ talkName, speakerName, description }) => {
         whileHover={{ scale: 1.03, transition: { duration: 0.2 } }}
         whileTap={{ scale: 0.99, transition: { duration: 0.2 } }}
         onClick={() => open()}
-        className='flex items-center max-w-full py-4 transition-all border border-gray-900 divide-x-2 rounded-lg lg:max-w-md grow lg:grow-0 bg-gradient-to-tl from-gray-200 to-blue-50 dark:bg-gradient-to-tl dark:from-gray-900 dark:to-blue-900 dark:active:bg-gradient-to-tl dark:active:from-gray-900 dark:active:to-blue-800 min-h-[10rem]'
+        className='flex items-center max-w-full py-4 transition-all border border-gray-900 divide-x-2 rounded-lg lg:max-w-md grow lg:grow-0 bg-gradient-to-tl from-gray-200 to-blue-50 dark:bg-gradient-to-tl dark:from-gray-900 dark:to-blue-900 dark:active:bg-gradient-to-tl dark:active:from-gray-900 dark:active:to-blue-800 min-h-[10rem] dark:border-gray-50'
       >
         <p className='px-8'>10:30am</p>
         <div className='flex flex-col items-start justify-around h-full px-8 space-y-2'>
-          <h3 className='text-left'>{talkName}</h3>
+          <h3 className='text-xl text-left font-title'>{talkName}</h3>
           <p className='font-black leading-relaxed'>{speakerName}</p>
         </div>
       </motion.button>
@@ -34,7 +35,7 @@ export const EventCard = ({ talkName, speakerName, description }) => {
       >
         {modalIsOpen && (
           <Modal modalIsOpen={modalIsOpen} handleClose={close}>
-            <div>{description}</div>
+            <EventModal startingTalk={id} />
           </Modal>
         )}
       </AnimatePresence>
