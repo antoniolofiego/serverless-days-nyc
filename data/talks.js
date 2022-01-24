284c18f359e4e7e3e25e4f0235839bdadd0c8b1c
Antonio Lo Fiego <alofiego@hey.com>
2022-01-24 02:04:20 -0500

    Add mock data for testing

--- data/talks.js
+++ data/talks.js
@@ -1,9 +1,9 @@
 export const talks = [
   {
     id: 0,
-    talkName: 'Test1',
-    speakerName: 'Speaker1',
-    description: 'Description1',
+    talkName: 'Creating a Serverless E-Commerce Stack with Observations',
+    speakerName: 'Michael Michaels',
+    description: 'A very cool piece of stuff.',
   },
   {
     id: 1,
@@ -13,13 +13,13 @@ export const talks = [
   },
   {
     id: 2,
-    talkName: 'Test3',
+    talkName: 'Creating a Serverless E-Commerce Stack with Observations',
     speakerName: 'Speaker3',
     description: 'Description3',
   },
   {
     id: 3,
-    talkName: 'Test4',
+    talkName: 'Creating a Serverless',
     speakerName: 'Speaker4',
     description: 'Description4',
   },
@@ -31,6 +31,18 @@ export const talks = [
   },
   {
     id: 5,
+    talkName: 'Test6',
+    speakerName: 'Speaker6',
+    description: 'Description6',
+  },
+  {
+    id: 6,
+    talkName: 'Test6',
+    speakerName: 'Speaker6',
+    description: 'Description6',
+  },
+  {
+    id: 7,
     talkName: 'Test6',
     speakerName: 'Speaker6',
     description: 'Description6',
