3cd8018752ca52150835fcad54a12551f3b680ef
Antonio Lo Fiego <alofiego@hey.com>
2022-02-17 12:40:58 -0500

    Change timeline layouts

--- components/TimelineItem.jsx
+++ components/TimelineItem.jsx
@@ -34,15 +34,19 @@ export const TimelineItem = ({ dateCopy, nextDate }) => {
       className='relative flex items-center h-full group'
       aria-disabled={stepIsOver}
     >
-      <div className={`hidden w-1/4 lg:block ${stepIsOver && 'text-gray-500'}`}>
-        <div
-          className={`space-x-1 md:flex group-hover:text-gray-700 dark:group-hover:text-gray-300 ${
+      <div
+        className={`hidden w-1/4 max-w-content ${
+          stepIsOver && 'text-gray-500'
+        }`}
+      >
+        <p
+          className={`space-x-1 group-hover:text-gray-700 dark:group-hover:text-gray-300 ${
             stepIsOver &&
             '!group-hover:text-gray-500 !text-gray-500 !dark:group-hover:text-gray-500'
           }`}
         >
           {monthNames[stepDate.getMonth()]}, {stepDate.getDate()}
-        </div>
+        </p>
         <div
           className={`italic font-bold group-hover:text-gray-700 dark:group-hover:text-gray-300 stepIsOver &&
             '!group-hover:text-gray-500 !text-gray-500 !dark:group-hover:text-gray-500'
@@ -53,7 +57,7 @@ export const TimelineItem = ({ dateCopy, nextDate }) => {
       </div>
 
       <div
-        className={`absolute z-10 h-full border-r-2 top-2 lg:left-32 left-8 border-gray-900 dark:border-gray-50 group-hover:border-gray-700 dark:group-hover:border-gray-300 ${
+        className={`absolute z-10 h-full border-r-2 top-2 left-8 border-gray-900 dark:border-gray-50 group-hover:border-gray-700 dark:group-hover:border-gray-300 ${
           stepIsOver && '!border-gray-500 !dark:border-gray-500 border-dotted'
         } ${stepIsOngoing && 'border-dashed'}`}
       >
@@ -65,11 +69,14 @@ export const TimelineItem = ({ dateCopy, nextDate }) => {
       </div>
 
       <div className={`pr-8 ml-16 ${stepIsOver && 'text-gray-500'}`}>
-        <p className='mb-2 text-sm lg:hidden'>
+        <p className='mb-2 text-sm'>
           {monthNames[stepDate.getMonth()]}, {stepDate.getDate()} 2022
         </p>
         <h3 className='mb-4 text-xl font-bold'>{title}</h3>
-        <p className='mb-8 text-lg'>{description}</p>
+        <div
+          className='mb-8'
+          dangerouslySetInnerHTML={{ __html: description }}
+        />
       </div>
     </div>
   );
