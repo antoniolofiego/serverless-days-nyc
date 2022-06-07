b13885136f09bda4dc10b76055785ef9c7eb8b8f
Antonio Lo Fiego <alofiego@hey.com>
2022-06-07 22:51:21 +0200

    Fix minimun Event Card sizing

--- components/EventCard.jsx
+++ components/EventCard.jsx
@@ -20,7 +20,7 @@ export const EventCard = ({ talkName, speaker, time, id }) => {
         whileHover={{ scale: 1.03, transition: { duration: 0.2 } }}
         whileTap={{ scale: 0.99, transition: { duration: 0.2 } }}
         onClick={() => open()}
-        className='flex items-center max-w-full py-4 transition-all border border-gray-900 divide-x-2 rounded-lg lg:max-w-md grow lg:grow-0 bg-gradient-to-tl from-gray-200 to-blue-50 dark:bg-gradient-to-tl dark:from-gray-900 dark:to-blue-900 dark:active:bg-gradient-to-tl dark:active:from-gray-900 dark:active:to-blue-800 min-h-[10rem] dark:border-gray-50'
+        className='flex items-center max-w-full py-4 transition-all border border-gray-900 divide-x-2 rounded-lg lg:max-w-md grow lg:grow-0 bg-gradient-to-tl from-gray-200 to-blue-50 dark:bg-gradient-to-tl dark:from-gray-900 dark:to-blue-900 dark:active:bg-gradient-to-tl dark:active:from-gray-900 dark:active:to-blue-800 min-h-[10rem] dark:border-gray-50 w-full'
       >
         <div className='flex justify-center w-1/4'>
           <p className='whitespace-nowrap'>{time}</p>
