0a6d393b9bfec847966f846eb18354b25e707bc5
Antonio Lo Fiego <alofiego@hey.com>
2022-06-07 21:02:40 +0200

    Add specification of talks to agenda

--- components/Agenda.jsx
+++ components/Agenda.jsx
@@ -7,7 +7,7 @@ export const Agenda = () => {
       className='flex flex-col items-center max-w-md mx-auto scroll-mt-28 sm:max-w-xl lg:max-w-5xl'
       id='agenda'
     >
-      <Heading title='Agenda' />
+      <Heading title='Talks Agenda' />
       <ul className='flex flex-wrap items-stretch justify-center grid-cols-1 gap-4 mx-auto place-items-center center lg:grid-cols-2 lg:justify-center'>
         {talks.map((talk, i) => {
           return (
